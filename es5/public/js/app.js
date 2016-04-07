19e808faee3e1b74311b55edc146973a0aa0adac
Hugo A Guerra <guerrahugoa@gmail.com>
2016-04-07 10:10:25 -0700

    initialized js and html files

---
+++ es5/public/js/app.js
@@ -0,0 +1,61 @@
+//coder constructor and methods
+
+function Coder (name, skill) {
+  this.name = name;
+  this.skill = skill;
+  this.hp = 111;
+};
+
+Coder.prototype.sarcasm = function(target) {
+  target.takeSarcasm(this.skill);
+}
+
+Coder.prototype.takeSarcasm = function (damage) {
+  this.hp -= damage;
+}
+
+//UberCoder constructor and methods
+
+function UberCoder (name, skill, uberskill) {
+  Human.call(this, name, skill);
+  this.uberskill = uberskill;
+}
+
+UberCoder.prototype = new Coder();
+
+UberCoder.prototype.useUberSkill = function(target) {
+  target.takeSarcasm(this.uberskill);
+}
+
+//player instances
+var reginald = new Coder('reginald coder', 12);
+var mattU = new UberCoder('matt uber coder', 32, 50);
+var kelly = new Coder ('kelly coder', 10);
+var maryU = new UberCoder('tom uber coder', 30, 51);
+
+//win lose function
+function winLose (hitter, target) {
+  if (target.hp <= 0) {
+    console.log(target.name + 'loses!' + hitter.name + 'wins!');
+  }
+}
+
+//weapons
+
+var cssJoke = 3;
+var consoleJoke = 4;
+var semicolonJoke = 5;
+
+//powerups
+
+var redbull = 3;
+var coffee = 4;
+var unfoundedSelfSatisfaction = 5;
+
+function addPower(coder, weapon) {
+  coder.skill += weapon;
+}
+
+function addHp(coder, powerup) {
+  coder.hp += powerup;
+}
