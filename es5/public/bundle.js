611d0c88601e92709e36d69a034955b902b14060
Hugo A Guerra <guerrahugoa@gmail.com>
2016-04-07 20:11:07 -0700

    configuring frontend, test pending

--- es5/public/bundle.js
+++ es5/public/bundle.js
@@ -50,10 +50,24 @@
 	var app = angular.module('app', []);
 	app.controller('RenderController', function() {
 	  this.welcomeText = 'Welcome to coder bicker bash! This is a text game war between coders and uber coders. Read what appears here, and submit your command in the input field below. Acceptable commands are "get", "attack" and a name to choose. Type "yes" and submit when you are ready to start';
-	  this.inputText = 'Type Here'
+	  this.inputText = 'Type Here';
 
 	  this.clearText = function() {
 	    this.welcomeText = null;
+	  }
+
+	  this.handleSubmit = function(input) {
+	    if (input == 'yes') {
+	        this.welcomeText = 'We are glad you chose to play. who would you like to be? choose "reginald", "matt", "mary" or "kelly"';
+	    } else if (input == 'reginald') {
+	        this.welcomeText = 'great choice, reginald!';
+	    } else if (input == 'matt') {
+	        this.welcomeText = 'great choice, matt!';
+	    } else if (input == 'mary') {
+	        this.welcomeText = 'great choice, mary!';
+	    } else if (input == 'kelly') {
+	        this.welcomeText = 'great choice, kelly!';
+	    }
 	  }
 	})
 
@@ -30812,9 +30826,9 @@
 
 	//player instances
 	var reginald = new Coder('reginald coder', 12);
-	var mattU = new UberCoder('matt uber coder', 32, 50);
+	var matt = new UberCoder('matt uber coder', 32, 50);
 	var kelly = new Coder ('kelly coder', 10);
-	var maryU = new UberCoder('tom uber coder', 30, 51);
+	var mary = new UberCoder('tom uber coder', 30, 51);
 
 	//win lose function
 	function winLose (hitter, target) {
--- es5/public/js/ang-app.js
+++ es5/public/js/ang-app.js
@@ -4,9 +4,23 @@ require(__dirname + '/app.js');
 var app = angular.module('app', []);
 app.controller('RenderController', function() {
   this.welcomeText = 'Welcome to coder bicker bash! This is a text game war between coders and uber coders. Read what appears here, and submit your command in the input field below. Acceptable commands are "get", "attack" and a name to choose. Type "yes" and submit when you are ready to start';
-  this.inputText = 'Type Here'
+  this.inputText = 'Type Here';
 
   this.clearText = function() {
     this.welcomeText = null;
+  }
+
+  this.handleSubmit = function(input) {
+    if (input == 'yes') {
+        this.welcomeText = 'We are glad you chose to play. who would you like to be? choose "reginald", "matt", "mary" or "kelly"';
+    } else if (input == 'reginald') {
+        this.welcomeText = 'great choice, reginald!';
+    } else if (input == 'matt') {
+        this.welcomeText = 'great choice, matt!';
+    } else if (input == 'mary') {
+        this.welcomeText = 'great choice, mary!';
+    } else if (input == 'kelly') {
+        this.welcomeText = 'great choice, kelly!';
+    }
   }
 })
